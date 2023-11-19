f68b49b8d3dbb084ec18c59bbaf5a1acd3c89682
chandramouli-lanka <143307509+chandramouli-lanka@users.noreply.github.com>
2023-11-19 14:08:59 +0530

    faq page done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}",
+    "./src/**/*.{html,js,jsx}",
     "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
