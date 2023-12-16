36870b66a7d23e4dd330812d9d7e120abd116ebb
Aarushi Agarwal <aarushiagarwal264@gmail.com>
2023-12-16 21:48:47 +0000

    Update 8 files

---
+++ src/components/events/Events.js
@@ -0,0 +1,51 @@
+// Import necessary libraries and styles
+import React from "react";
+import { motion } from "framer-motion";
+import "./App.css"; // Import your CSS file
+import img2 from "../../assets/carousel1.jpg";
+import img1 from "../../assets/carousel2.jpg";
+
+// Sample data for events
+const eventsData = [
+  {
+    id: 1,
+    image: img1,
+    description: "Description for Event 1",
+  },
+  {
+    id: 2,
+    image: img2,
+    description: "Description for Event 2",
+  },
+  // Add more events as needed
+];
+
+const Events = () => {
+  return (
+    <div className="events-container">
+      <h1 className="events-title">
+        Upcoming <span style={{ color: "black" }}>Events</span>
+      </h1>
+
+      <div className="events-grid">
+        {eventsData.map((event) => (
+          <motion.div
+            key={event.id}
+            className="event-card"
+            whileHover={{ scale: 1.09 }}
+            whileTap={{ scale: 0.95 }}
+          >
+            <motion.img
+              src={event.image}
+              alt={`Event ${event.id}`}
+              whileHover={{ scale: 1.09 }}
+            />
+            <p className="event-description">{event.description}</p>
+          </motion.div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Events;
